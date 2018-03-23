db983b4acb7e4aea5d6d870a4601f00fe6ceb545
Nishant Jain <nishantkumarajain@gmail.com>
2018-03-23 11:14:28 +0530

    Added additional loaders to bundle Angular Styles and Templates

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
  mode: 'development',
  entry: {
    polyfills: path.resolve(__dirname, './polyfills.ts'),
+   styles: './styles.css',
    app: path.resolve(__dirname, './app.module.ts'),
  },
  output: {
@@ -14,11 +15,13 @@ module.exports = {
  },
  module: {
    rules: [
-     { test: /\.ts/, use: 'ts-loader' }
+     { test: /\.ts/, use: ['ts-loader', 'angular2-template-loader'] },
+     { test: /\.html$/, use: 'raw-loader' },
+     { test: /\.css$/, use: ['to-string-loader', 'style-loader', 'css-loader'] },
    ]
  },
  resolve: {
-  extensions: [".js", ".ts"],
+  extensions: [".js", ".ts", ".html", ".css", ".ico"],
  },
  plugins: [
    new webpack.ContextReplacementPlugin(
